fae19efbaa68ff1073ff5e449326a86f44f470b8
fenlon <1129507011@qq.com>
2022-05-11 17:58:05 +0800

    单文件组件

---
+++ src/组件/单文件组件/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <School></School>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import School from './School.vue'
+
+export default {
+    name: 'App',
+    template:`<App></App>`,
+    components:{
+        School
+    }
+}
+</script>
+
+<style>
+
+</style>
